680b9934745b59385d656e956557ad4a0e7fc99a
pagerdown <amirahdumps@gmail.com>
2026-06-01 22:15:41 +0000

    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials using python
+# Assignment Number: 1
+#
+# Name: Amirah Whitney Amoah
+# STUDENT ID: 2425401759
+# Email: 2425401759@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Amirah Whitney Amoah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Print initials AWA in small form then also as block letters made of their own letter
 
 
+    print()
+print("...AWA")
+print()
+
+print("...AAAAAAAAAA...WWW    WWWW...AAAAAAAAAA")
+print("...AA      AA...WWW    WWWW...AA      AA")
+print("...AA      AA...WWW    WWWW...AA      AA")
+print("...AA      AA...WWW WW WWWW...AA      AA")
+print("...AAAAAAAAAA...WWWWWWWWWWW...AAAAAAAAAA")
+print("...AA      AA...WWWW   WWWW...AA      AA")
+print("...AA      AA...WWWW   WWWW...AA      AA")
+print("...AA      AA...WWWW   WWWW***AA      AA")
+print("...AA      AA...WWWW   WWWW***AA      AA")
+print("...AA.     AA...WWWW   WWWW***AA.     AA")
+
+print()
 
 main()
